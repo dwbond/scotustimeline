38a57e9e850800b8477353d2ea0d7f9af9f75222
Daniel Bond <daniel@dbond.cc>
2012-11-16 23:44:01 -0500

    Added beginning methods for timeline, modified timeline file

--- timeline/views.py
+++ timeline/views.py
@@ -6,6 +6,21 @@ from django.template import RequestContext
 
 cases = Cases.objects.all()
 
+def timespan(start, end):
+    # expects dates
+    interval = end - start
+    divided = interval / 6
+    interval_list = [start]
+
+    subdivision = 6
+    newtime = start
+    while subdivision < 6:
+        interval_list.append(newtime + divided)
+        newtime = newtime + divided
+        subdivision = subdivision - 1
+       
+    return interval_list
+
 def index(request):
 
     return render_to_response('timeline.html', {
